d94358ed2268b64ecfb6fd35d036baf4b373f21e
hemant097 <hem789ant1997@outlook.com>
2022-09-27 01:13:19 +0530

    implemented feedback

--- pscript.js
+++ pscript.js
@@ -1,3 +1,17 @@
+var button = document.querySelector("#navsmartphone");
+
+button.addEventListener("click", function(){
+    deleteItem();
+});
+
+function deleteItem(){
+    var x = document.getElementById("navbar_container");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
 
 
 
